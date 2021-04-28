1b6ccbcdddd2ae4dc02b9a0616c312401f04042e
nikolaos karapas <nikos.karapas86@gmail.com>
2021-04-28 10:01:51 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {Ionicons} from '@expo/vector-icons';
+import AddEntry from "./components/AddEntry";
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Provider store={createStore(reducer)}>
+    <View>
+      <AddEntry />
     </View>
+  </Provider>
   );
 }
 
